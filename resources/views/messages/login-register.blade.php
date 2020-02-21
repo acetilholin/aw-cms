f2edbf561d428798d220f0fe78b34fd49a5a8867
Gregor <kosmina.gregor@gmail.com>
2020-02-21 21:13:15 +0100

    improved server validation

--- resources/views/messages/login-register.blade.php
+++ resources/views/messages/login-register.blade.php
@@ -1,4 +1,3 @@
-
 @if ($message = Session::get('success'))
     <div class="alert alert-success" role="alert" id="message">
         {{ $message }}
@@ -8,5 +7,12 @@
 @if ($message = Session::get('error'))
     <div class="alert alert-danger" role="alert" id="message">
         {{ $message }}
+    </div>
+@endif
+@if ($errors->any())
+    <div class="alert alert-danger" role="alert" id="message">
+        @foreach ($errors->all() as $error)
+            {{ $error }}<br>
+        @endforeach
     </div>
 @endif
